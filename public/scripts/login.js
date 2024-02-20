f2c5cf7e1c1f393d6225e702c16269bb90223b1b
Mohammad Asad <94637256+ma-asad@users.noreply.github.com>
2024-02-20 11:34:30 +0400

    Refactor login and signup forms and styles

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,29 +1,44 @@
 export function getLoginForm() {
   return `
-        <h2>Wispi</h2>
-        <h4><i>"Wisp of Wisdom, Quotes of Illumination"</i></h4>
-        <form id="loginForm">
-            <input type="text" id="username" placeholder="Username" required>
-            <input type="password" id="password" placeholder="Password" required>
-            <input type="submit" value="Login">
-        </form>
-        <p>Don't have an account? <a href="#/signup">Sign up</a></p>
-    `;
+<div class="login-container">
+    <div class="login-form">
+            <h1>Wispi</h1>
+            <h2><quote>“Wisp of Wisdom, Quotes of \u2019I\u2019llumination”</quote></h2>
+            <form action="#" method="post">
+                <input type="text" id="username" name="username" placeholder="Enter your username or email" required>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required>
+                <button type="submit">Log In</button>
+                <p><a href="#">Forgotten your Password?</a></p>
+            </form>
+    </div>
+
+    <div class="login-form">
+        <p>Don't have an account? <a href="#/signup">Sign Up</a></p>
+    </div>
+</div>
+`;
 }
 
 export function getSignupForm() {
   return `
-        <h2>Wispi</h2>
-        <h4><i>"Wisp of Wisdom, Quotes of Illumination"</i></h4>
+<div class="login-container">
+    <div class="login-form">
+        <h1>Wispi</h1>
+        <h2><quote>“Wisp of Wisdom, Quotes of \u2019I\u2019llumination”</quote></h2>
         <h4><i>Sign Up to see, share and create Wispis</i></h4>
-        <form id="signupForm">
+
+        <form action="#" method="post">
             <input type="email" id="email" placeholder="Email" required>
             <input type="text" id="fullName" placeholder="Full Name" required>
             <input type="text" id="username" placeholder="Username" required>
             <input type="password" id="password" placeholder="Password" required>
             <input type="password" id="confirmPassword" placeholder="Confirm Password" required>
-            <input type="submit" value="Signup">
+            <button type="submit">Sign Up</button>
         </form>
+    </div>
+    <div class="login-form">
         <p>Have an account? <a href="#/login">Log in</a></p>
-    `;
+    </div>
+</div>
+`;
 }
