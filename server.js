e5c6893ec3febb1345f7346313b1b194d2201b00
Mohammad Asad <94637256+ma-asad@users.noreply.github.com>
2024-03-13 14:36:04 +0400

    Refactor server.js: Remove unused routes

--- server.js
+++ server.js
@@ -6,35 +6,34 @@ const uri =
   "mongodb+srv://asad:LcKJMBT6krGoMnt0@wispi.sy0u7sr.mongodb.net/?retryWrites=true&w=majority&appName=Wispi";
 const app = express();
 
-app.use(express.json());
-express.static("./public");
+app.use(express.static("./public"));
 
-app.post("/M00952726/user", async (req, res) => {
-  const user = {
-    name: req.body.name,
-    email: req.body.email,
-    studentID: req.body.studentID,
-  };
-  const database = client.db("Wispi");
-  const collection = database.collection("users");
-  try {
-    const result = await collection.insertOne(user);
-    res.status(201).json(result);
-  } catch (error) {
-    res.status(500).json({ message: "Error inserting user" });
-  }
-});
+// app.post("/M00952726/user", async (req, res) => {
+//   const user = {
+//     name: req.body.name,
+//     email: req.body.email,
+//     studentID: req.body.studentID,
+//   };
+//   const database = client.db("Wispi");
+//   const collection = database.collection("users");
+//   try {
+//     const result = await collection.insertOne(user);
+//     res.status(201).json(result);
+//   } catch (error) {
+//     res.status(500).json({ message: "Error inserting user" });
+//   }
+// });
 
-app.get("/M00952726/users", async (req, res) => {
-  const database = client.db("Wispi");
-  const collection = database.collection("users");
-  try {
-    const result = await collection.find().toArray();
-    res.status(200).json(result);
-  } catch (error) {
-    res.status(500).json({ message: "Error getting users" });
-  }
-});
+// app.get("/M00952726/users", async (req, res) => {
+//   const database = client.db("Wispi");
+//   const collection = database.collection("users");
+//   try {
+//     const result = await collection.find().toArray();
+//     res.status(200).json(result);
+//   } catch (error) {
+//     res.status(500).json({ message: "Error getting users" });
+//   }
+// });
 
 
 const client = new MongoClient(uri, {
