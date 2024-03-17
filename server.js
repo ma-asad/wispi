a5c61a8a3bed8b410a2dc273948628c8ba718826
Mohammad Asad <94637256+ma-asad@users.noreply.github.com>
2024-03-17 07:48:59 +0400

    Add express.json middleware to server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Path: server.js
+
 import { MongoClient, ServerApiVersion } from "mongodb";
 import express from "express";
 
@@ -6,34 +8,8 @@ const uri =
   "mongodb+srv://asad:LcKJMBT6krGoMnt0@wispi.sy0u7sr.mongodb.net/?retryWrites=true&w=majority&appName=Wispi";
 const app = express();
 
+app.use(express.json());
 app.use(express.static("./public"));
-
-// app.post("/M00952726/user", async (req, res) => {
-//   const user = {
-//     name: req.body.name,
-//     email: req.body.email,
-//     studentID: req.body.studentID,
-//   };
-//   const database = client.db("Wispi");
-//   const collection = database.collection("users");
-//   try {
-//     const result = await collection.insertOne(user);
-//     res.status(201).json(result);
-//   } catch (error) {
-//     res.status(500).json({ message: "Error inserting user" });
-//   }
-// });
-
-// app.get("/M00952726/users", async (req, res) => {
-//   const database = client.db("Wispi");
-//   const collection = database.collection("users");
-//   try {
-//     const result = await collection.find().toArray();
-//     res.status(200).json(result);
-//   } catch (error) {
-//     res.status(500).json({ message: "Error getting users" });
-//   }
-// });
 
 
 const client = new MongoClient(uri, {
@@ -61,3 +37,4 @@ function startServer() {
 }
 
 run();
+
